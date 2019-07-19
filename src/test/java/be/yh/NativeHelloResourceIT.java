6a90f722a873d65edcb4223df8c04c88618a307f
yassin.hajaj <yassin.hajaj@hotmail.com>
2019-07-20 00:05:29 +0200

    Initial Commit

---
+++ src/test/java/be/yh/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package be.yh;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
